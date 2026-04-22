9c6e8cc3837aa5818c28b7620872ac42d1e5d0f0
BillyOWasTaken <billy.ontoseno@gmail.com>
2026-04-23 00:42:25 +0700

    feat: auction and buildingmanager

---
+++ include/models/Auction.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include "Property.hpp"
+#include "Player.hpp"  
+
+class Property;
+class Player;
+
+class Auction {
+private:
+    std::weak_ptr<Property> property;
+    int currentBid;
+    std::weak_ptr<Player> currentBidder;
+    int consecutivePassCount;
+
+    std::vector<std::weak_ptr<Player>> participants;
+    bool active;
+
+public:
+    Auction(std::shared_ptr<Property> property,
+            const std::vector<std::shared_ptr<Player>>& players);
+
+    void startAuction();
+
+    bool placeBid(std::shared_ptr<Player> player, int bidAmount);
+    void pass(std::shared_ptr<Player> player);
+
+    bool isAuctionEnded() const;
+    std::shared_ptr<Player> getWinner() const;
+};
