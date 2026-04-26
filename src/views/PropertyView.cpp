03c942390e250885c942edf93871f58af8a36be2
Kacang Tojin <amanfauzan31@gmail.com>
2026-04-27 00:49:07 +0700

    cetak akta

--- src/views/PropertyView.cpp
+++ src/views/PropertyView.cpp
@@ -1,16 +1,85 @@
 #include "../../include/views/PropertyView.hpp"
+#include "../../include/models/Street.hpp"
+#include "../../include/models/Railroad.hpp"
+#include "../../include/models/Utility.hpp"
 
 #include <iostream>
 #include <iomanip>
 #include <map>
+#include <sstream>
 
 using namespace std;
+
+static string formatMoney(int amount) {
+    string s = to_string(amount);
+    string out;
+    int count = 0;
+    for (int i = (int)s.size() - 1; i >= 0; --i) {
+        out.insert(out.begin(), s[i]);
+        if (++count % 3 == 0 && i > 0) out.insert(out.begin(), '.');
+    }
+    return "M" + out;
+}
+
+static void printRow(const string& label, const string& value) {
+    string line = "| " + label;
+    if ((int)line.size() < 21) line += string(21 - line.size(), ' ');
+    line += ": " + value;
+    if ((int)line.size() < 33) line += string(33 - line.size(), ' ');
+    line += "|";
+    cout << line << "\n";
+}
 
 void PropertyView::showPropertyDetail(Property* property) {
     if (!property) return;
 
-    cout << "Property: " << property->getName() << endl;
-    cout << "Owner   : " << property->getOwner() << endl;
+    string category = property->getColor();
+    string name = property->getName();
+    string code = property->getCode();
+
+    cout << "+================================+\n";
+    cout << "|        AKTA KEPEMILIKAN        |\n";
+
+    string title = "|    [" + category + "] " + name + " (" + code + ")";
+    if ((int)title.size() < 33) title += string(33 - title.size(), ' ');
+    title += "|";
+    cout << title << "\n";
+
+    cout << "+================================+\n";
+    printRow("Harga Beli", formatMoney(property->getPurchasePrice()));
+    printRow("Nilai Gadai", formatMoney(property->getMortgageValue()));
+    cout << "+--------------------------------+\n";
+
+    if (Street* s = dynamic_cast<Street*>(property)) {
+        printRow("Sewa (unimproved)", formatMoney(s->getRent(0)));
+        printRow("Sewa (1 rumah)",    formatMoney(s->getRent(1)));
+        printRow("Sewa (2 rumah)",    formatMoney(s->getRent(2)));
+        printRow("Sewa (3 rumah)",    formatMoney(s->getRent(3)));
+        printRow("Sewa (4 rumah)",    formatMoney(s->getRent(4)));
+        printRow("Sewa (hotel)",      formatMoney(s->getRent(5)));
+        cout << "+--------------------------------+\n";
+        printRow("Harga Rumah", formatMoney(s->getHousePrice()));
+        printRow("Harga Hotel", formatMoney(s->getHotelPrice()));
+    } else if (Railroad* r = dynamic_cast<Railroad*>(property)) {
+        for (int i = 1; i <= 4; ++i) {
+            printRow("Sewa (" + to_string(i) + " stasiun)", formatMoney(r->getRent(i)));
+        }
+    } else if (Utility* u = dynamic_cast<Utility*>(property)) {
+        printRow("Sewa (1 utility)", to_string(u->getRent(1, 1)) + " x dadu");
+        printRow("Sewa (2 utility)", to_string(u->getRent(1, 2)) + " x dadu");
+    }
+
+    cout << "+================================+\n";
+
+    string status;
+    if (property->getStatus() == StatusType::OWNED)
+        status = "OWNED (" + property->getOwner() + ")";
+    else if (property->getStatus() == StatusType::MORTGAGED)
+        status = "MORTGAGED";
+    else
+        status = "BANK";
+    printRow("Status", status);
+    cout << "+================================+\n";
 }
 
 void PropertyView::showPlayerProperties(Player* player, const vector<unique_ptr<Tile>>& tiles) {
